605a4a0d479d06237878908d584f415fa2ed6194
soyeonh130 <soyeonh130@gmail.com>
2026-07-12 03:52:16 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 function goMBTI() {
-    location.href = 'https://www.career.go.kr/cnet/front/examen/inspctMain.do';
+    // 요청하신 다중지능검사 공식 링크로 올바르게 수정했습니다.
+    location.href = 'https://multiiqtest.com';
 }
